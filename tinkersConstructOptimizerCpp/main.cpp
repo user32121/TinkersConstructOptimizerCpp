4d6cb19211a011d3646f275a98a594e70c487f6d
Jupiter Peng <pengjupiter5@gmail.com>
2021-09-21 12:57:38 -0700

    Initial Commit

---
+++ tinkersConstructOptimizerCpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tinkersConstructOptimizerCpp
+//
+//  Created by Jupiter Peng on 9/21/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
